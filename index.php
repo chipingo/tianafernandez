d0a5a97854b61b1a9e13feb57c9ec4b3b72b17e9
chipingo1 <david@nygts.com>
2017-03-16 02:33:12 -0400

    asset add commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Tiana Fernandez</title>
+    </head>
+    <body>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+
+        <link rel="stylesheet" href="assets/stylesheets/bootstrap.css">
+        <link rel="stylesheet" href="assets/stylesheets/style.css">
+
+
+
+            <div class="culo1">
+            <h1 class="text-center h1customtxtcolor " >Tiana Fernandez</h1>
+        </div>
+            <div class="">
+            <p4>This site is currently under construction</p4>
+            <div class="container-fluid" "col-md12" >
+                <a class="btn btn-success h1customtxtcolor" href="http://google.com">go to Google</a>
+            </div>
+            <br>
+            <div class="container-fluid" "col-md12">
+                <a class="btn btn-primary h1customtxtcolor" href="http://facebook.com">go to Facebook</a>
+            </div>
+            <br>
+            <div class="container-fluid" "col-md12">
+                <a class="btn btn-success h1customtxtcolor" href="http://yahoo.com">go to Yahoo</a>
+            </div>
+
+
+        </div>
+
+
+    </body>
+</html>
